18ea794adda2a96ac014763d9cdea489ef8b09b1
nandz07 <nandakumart07@gmail.com>
2022-09-05 11:22:38 +0530

    tried something extra and made kulam

--- js/js/myscript.js
+++ js/js/myscript.js
@@ -1,6 +1,17 @@
 a='10';
 b=20;
 c=a+b;//string concatenation
-console.log(c);
+console.log("value of c "+c);
 names=['appu','anu','abi','mani','anvar'];
-console.log(names);
+console.log("names in array are "+names);
+console.log("anna".toUpperCase())
+console.log(names[0].toUpperCase());
+d="abcd".toUpperCase().length //method chaining
+console.log(d);
+console.log(c.bold());
+p=document.getElementById("title").innerText;
+//alert(p);
+
+function CalcAdd(){
+    document.getElementById("title").innerText = "Welcome to JavaScript".bold();
+}
