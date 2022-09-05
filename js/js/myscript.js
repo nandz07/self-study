47ea2d38b35c2cf5051e15400d1c2a36e76937d0
nandz07 <nandakumart07@gmail.com>
2022-09-05 13:43:42 +0530

    tintu vlog js vd 6(array in js) done->vd 7(objects in js))

--- js/js/myscript.js
+++ js/js/myscript.js
@@ -8,13 +8,14 @@ console.log("anna".toUpperCase())
 console.log(names[0].toUpperCase());
 d="abcd".toUpperCase().length //method chaining
 console.log(d);
-console.log(c.bold());
-p=document.getElementById("title").innerText;
+console.log(c.bold());//ith nokkanda njan oru parrekshanam nadathiyatha(Don't Care)
+p=document.getElementById("title").innerText;//Don't Care(DC)
 //alert(p);
 
-function CalcAdd(){
-    document.getElementById("title").innerText = "Welcome to JavaScript".bold();
+function CalcAdd(){//(DC)
+    document.getElementById("title").innerText = "Welcome to JavaScript".bold();//(DC)
 }
+//************Srting******* */
 j="hello    "
 console.log("before trim "+j.length);
 console.log("after trim "+j.trim().length);
@@ -43,3 +44,29 @@ console.log("join with '/' => "+str4)
 
 str5=str.split(" ").join("/");
 console.log(str5)
+
+// *********Array***********
+
+ar=[10,20,30,40,50,60,70,80,90,100]
+console.log(ar);
+console.log("index of 50 => "+ar.indexOf(50));
+console.log("index of 200 => "+ar.indexOf(200))
+
+ar.push("test")//insert into last position of array
+console.log("insert new value at end => "+ar);
+
+ar[4]=500
+console.log("ar[4]=500 => "+ar)
+
+ar.pop()
+console.log("delete last value = >"+ar);//delete last one onthe array
+//can do operation on the concept of stack
+
+ar.shift()
+console.log("delete vale from front => "+ar)
+
+ar.unshift(1000)
+console.log("added value to front of an array => "+ar);
+
+ar.splice(0,2)
+console.log("remove 0-2 indexed numbers => "+ar);
